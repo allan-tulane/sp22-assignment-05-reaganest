cd13a334467e78279c7d54ddd580bf0881671546
Reagan Esteves <89606813+reaganest@users.noreply.github.com>
2022-04-29 02:31:08 +0000

    somewhat finished

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Assignment 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Reagan Esteves
 
 
 Place all written answers from `assignment-05.md` here for easier grading.
@@ -11,14 +11,23 @@ Place all written answers from `assignment-05.md` here for easier grading.
 
 
 - **1a.**
+ This algorithm is a greedy algorithm  that is used to pick the largest Geometrica denomination available. The cycle drops from lower and lower through each denomination until it cannot no more,the cycle will proceed until it has the desired amount of change it needs. The largest denomination is always apart of the set so it produces few coins making it an optimal algorithm
+- **1b.**
+ Work is O(log_2(n))
+  Span is O(log_2(n))
+- **2a.**
 
-
-
-
+A counter example is with the denominations {1,5,6,9}, it is optimal to get 11 through 5 and 6. with 2 coins, but a greedy algorithm would choose 9, and 1 twice. This would lead to the greedy algorithms return 3 coins instead of 2. So in this case, the greedy can't work optimally. 
 
 
 
 - **2b.**
 
+If there are c number of coins then:
+Work is O(n*c)
+Span is O(n*c)
 
 
+- **3a.**
+The optimal substructre property for this verison of the edit distance is
+first when  S[0] = T[0], then MED(S[1:], T[1:]) then when S[0] does not equal T[0], then 1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:],T[1:]))
--- main.py
+++ main.py
@@ -5,25 +5,50 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('book', 'back'), ('kookaburra', 'kookybird-'), ('relev-ant','-elephant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
+    if (S == "") and (T != ""):
         return(len(T))
-    elif (T == ""):
+    elif (T == "") and (S != ""):
         return(len(S))
+    elif (S == "") and (T == ""):
+        return 0
     else:
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return (1 + min(MED(S, T[1:]), MED(S[1:], T),MED(S[1:],T[1:])))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement memoization
-    pass
-
+  key = S,T
+  if not S:
+    MED[0,len(T)] = len(T)
+    return MED[0,len(T)]
+  elif not T:
+    MED[len(S),0] = len(S)
+    return MED[len(S),0]
+  elif key in MED:
+    return MED[key]
+  elif (S[0] == T[0]):
+    return(fast_MED(S[1:], T[1:]))
+  else:
+    MED[key] = (1 + min(fast_MED(S, T[1:]), fast_MED(S[1:],T), fast_MED(S[1:],T[1:])))
+    return MED[key]
+    
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    key = S,T
+  if not S:
+    MED[0,len(T)] = len(T)
+    return MED[0,len(T)]
+  elif not T:
+    MED[len(S),0] = len(S)
+    return MED[len(S),0]
+  elif key in MED:
+    return MED[key]
+  elif (S[0] == T[0]):
+    return(fast_MED(S[1:], T[1:]))
+  else:
+    MED[key] = (1 + min(fast_MED(S, T[1:]), fast_MED(S[1:],T), fast_MED(S[1:],T[1:])))
+    return MED[key]
 
 def test_MED():
     for S, T in test_cases:
